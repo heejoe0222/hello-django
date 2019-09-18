3e5a5fb74f840f5385df22006ac410d9c81ecc3b
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2019-09-19 07:12:54 +0900

    Finished Part4 practice

--- polls/views.py
+++ polls/views.py
@@ -2,34 +2,25 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-#from django.views import generic
+from django.views import generic
 
 from django.db.models import F
 from .models import Question, Choice
 
+class IndexView(generic.ListView): ## ListView -> objct 목록 표시
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template =loader.get_template('polls/index.html')
-    context = { ## template 변수명과 python 객체 연결하는 dictionary
-        'latest_question_list': latest_question_list,
-    }
-    #return HttpResponse(template.render(context,request))
-    return render(request,'polls/index.html',context) ## HttpResponse, loader 안 써도 됨
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    '''
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    '''
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,'polls/detail.html', {'question':question})
+class DetailView(generic.DetailView): ## DetailView -> 특정 object에 대한 세부 정보 페이지 표시
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,'polls/results.html', {'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
